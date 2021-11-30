71049890fee3eaf141d5371354a404d65075f8ae
Thomas Joly <thomas.joly@scality.com>
2021-11-30 15:58:05 +0100

    MS-23 new archi headers minesweeper module

--- src/minesweeper/minesweeper_unit-test.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MINESWEEPER_UNIT_TEST_H__
-#define __MINESWEEPER_UNIT_TEST_H__
-
-/**
- * This file defines all prototypes needed for the unit-tests
- * of this minesweeper module.
- */
-
-#endif /* !__MINESWEEPER_UNIT_TEST_H__ */
